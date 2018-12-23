fa11cc6a4fc6d7d7f7382ed4389bac278554ba7d
zxs <1501873102@qq.com>
2018-12-23 11:47:48 +0800

    新增顺序查找和插值查找

--- src/main/java/com/zxs/ssh/template/util/SearchUtil.java
+++ src/main/java/com/zxs/ssh/template/util/SearchUtil.java
@@ -14,6 +14,27 @@ package com.zxs.ssh.template.util;
 public class SearchUtil {
 
     /**
+     * 顺序查找
+     *
+     * @param a 待查找数组
+     * @param value 查找值
+     * @return 若找到，返回值的索引；若未找到，返回-1
+     */
+    public static int sequenceSearch(int[] a, int value){
+        int res = -1;
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] == value){
+                res = i;
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("顺序查找花费时间：" + (endTime - startTime) + "ms\t\t\t数组大小：" + a.length + "\t\t\t查找结果：" + value + "->" + (res > -1 ? a[res]:-1));
+        return res;
+    }
+
+    /**
      * 二分查找
      *
      * @param a          待查找数组（升序）
@@ -26,7 +47,8 @@ public class SearchUtil {
         int res = -1;
         long startTime = System.currentTimeMillis();
         while (leftIndex <= rightIndex) {
-            int midIndex = (leftIndex + rightIndex) / 2;
+            //int midIndex = (leftIndex + rightIndex) / 2;
+            int midIndex = leftIndex + (rightIndex - leftIndex)/2;
             if (a[midIndex] == value) {
                 res = midIndex;
                 break;
@@ -39,7 +61,40 @@ public class SearchUtil {
             }
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("二分查找花费时间：" + (endTime - startTime) + "ms\t\t\t数组大小：" + a.length + "\t\t\t查找结果(索引值)：" + res);
+        System.out.println("二分查找花费时间：" + (endTime - startTime) + "ms\t\t\t数组大小：" + a.length + "\t\t\t查找结果：" + value + "->" + (res > -1 ? a[res]:-1));
+        return res;
+    }
+
+    /**
+     * 插值查找, 插值查找是二分查找的优化
+     * midIndex = (leftIndex + rightIndex) / 2 即 midIndex = leftIndex + 1 / 2 * （rightIndex - leftIndex）
+     * 针对1/2做如下改进 1/2 -》 (value-a[leftIndex])/(a[rightIndex]-a[leftIndex])
+     * 查找值越接近于最小值，1/2越趋近于0；查找值越接近于最大值，1/2越趋近于1
+     *
+     * @param a          待查找数组（升序）
+     * @param leftIndex  查找范围左索引
+     * @param rightIndex 查找范围右索引
+     * @param value      查找值
+     * @return 若找到，返回值的索引；若未找到，返回-1
+     */
+    public static int insertSearch(int[] a, int leftIndex, int rightIndex, int value){
+        int res = -1;
+        long startTime = System.currentTimeMillis();
+        while (leftIndex <= rightIndex) {
+            int midIndex = leftIndex + (value-a[leftIndex])/(a[rightIndex]-a[leftIndex])*(rightIndex-leftIndex);
+            if (a[midIndex] == value) {
+                res = midIndex;
+                break;
+            }
+            if (a[midIndex] > value) {
+                rightIndex = midIndex - 1;
+            }
+            if (a[midIndex] < value) {
+                leftIndex = midIndex + 1;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("插值查找花费时间：" + (endTime - startTime) + "ms\t\t\t数组大小：" + a.length + "\t\t\t查找结果：" + value + "->" + (res > -1 ? a[res]:-1));
         return res;
     }
 
@@ -47,14 +102,17 @@ public class SearchUtil {
         int length = 10000000;
         int[][] a = new int[8][length];//8种排序算法的初始数据
         for (int i = 0; i < length; i++) {
-            a[0][i] = (new Double(Math.random() * 100)).intValue();
+            a[0][i] = (new Double(Math.random() * length)).intValue();
             for (int j = 1; j < 8; j++) {
                 a[j][i] = a[0][i];
             }
         }
-        SortUtil.radixSort(a[0], length);
-        //printArr(a[0]);
-        binarySearch(a[0], 0, length, (new Double(Math.random() * 100)).intValue());
+        SortUtil.heapSort(a[1], length);
+        SortUtil.heapSort(a[2], length);
+        int value = (new Double(Math.random() * length)).intValue();
+        //sequenceSearch(a[0], value);
+        binarySearch(a[1], 0, length-1, value);
+        insertSearch(a[2], 0, length-1, value);
     }
 
     /**
